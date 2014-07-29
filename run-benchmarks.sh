3259229edae6ccbb913046d87a51d4fb52858bb7
Adrian Papari <blackrax@gmail.com>
2014-07-29 13:40:50 +0200

    pom for entreri

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -2,4 +2,6 @@
 java -jar ashley-1.0.1/target/microbenchmarks.jar -o results/ashley-bench.csv -rf csv 
 java -jar artemis-odb-0.4.0/target/microbenchmarks.jar -o results/artemis-0.4.0-bench.csv -rf csv
 java -jar artemis-odb-0.6.5/target/microbenchmarks.jar -o results/artemis-0.6.5-bench.csv -rf csv
+java -jar entreri-1.7.0/target/microbenchmarks.jar -o results/artemis-0.6.5-bench.csv -rf csv
+java -jar gdx-artemis-0.5.0/target/microbenchmarks.jar -o results/artemis-0.6.5-bench.csv -rf csv
 
