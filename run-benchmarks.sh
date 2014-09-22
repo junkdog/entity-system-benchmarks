d77c81d5fb21d22c53f2d9a256789ef9b5b463d2
Adrian Papari <blackrax@gmail.com>
2014-09-22 20:35:14 +0200

    Ok. here goes

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -1,5 +1,11 @@
 #/bin/bash
-java -jar ashley-1.0.1/target/microbenchmarks.jar -o results/ashley-bench.csv -rf csv 
-java -jar artemis-odb-0.4.0/target/microbenchmarks.jar -o results/artemis-0.4.0-bench.csv -rf csv
-java -jar artemis-odb-0.6.5/target/microbenchmarks.jar -o results/artemis-0.6.5-bench.csv -rf csv
+
+# PARAMS="-i 1 -wi 1 -r 1 -p entityCount=4096"
+
+java -jar artemis-odb-0.4.0/target/microbenchmarks.jar -o results/artemis-odb-0.4.0.csv -rf csv $PARAMS
+java -jar artemis-odb-0.6.5/target/microbenchmarks.jar -o results/artemis-odb-0.6.5.csv -rf csv $PARAMS
+java -jar artemis-odb-0.7.0/target/microbenchmarks.jar -o results/artemis-odb-0.7.0.csv -rf csv $PARAMS
+java -jar ashley-1.0.1/target/microbenchmarks.jar -o results/ashley-1.0.1.csv -rf csv $PARAMS
+java -jar ashley-1.2.0/target/microbenchmarks.jar -o results/ashley-1.2.0.csv -rf csv $PARAMS
+java -jar gdx-artemis-0.5.0/target/microbenchmarks.jar -o results/gdx-artemis-0.5.0.csv -rf csv $PARAMS
 
