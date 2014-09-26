c30ec8070c5311114c20d607685e5e072f5ac7bb
Adrian Papari <blackrax@gmail.com>
2014-09-26 03:14:48 +0200

    improved shell script.

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -1,14 +1,18 @@
 #/bin/bash
 
+# for debugging:
 # PARAMS="-i 1 -wi 1 -r 1 -p entityCount=4096"
 
-mvn clean install
-java -jar artemis-odb-0.4.0/target/microbenchmarks.jar -o results/artemis-odb-0.4.0.csv -rf csv $PARAMS
-java -jar artemis-odb-0.6.5/target/microbenchmarks.jar -o results/artemis-odb-0.6.5.csv -rf csv $PARAMS
-java -jar artemis-odb-0.7.0/target/microbenchmarks.jar -o results/artemis-odb-0.7.0.csv -rf csv $PARAMS
-java -jar ashley-1.0.1/target/microbenchmarks.jar -o results/ashley-1.0.1.csv -rf csv $PARAMS
-java -jar ashley-1.2.0/target/microbenchmarks.jar -o results/ashley-1.2.0.csv -rf csv $PARAMS
-java -jar gdx-artemis-0.5.0/target/microbenchmarks.jar -o results/gdx-artemis-0.5.0.csv -rf csv $PARAMS
-mvn -Pfast clean install -f artemis-odb-0.7.0/pom.xml 
-java -jar artemis-odb-0.7.0/target/microbenchmarks.jar -o results/artemis-odb-0.7.0_fast.csv -rf csv $PARAMS
+function run_bench() {
+	java -jar $1/target/microbenchmarks.jar -rf json -rff results/$1.json $PARAMS | tee results/$1.log
+}
 
+mvn clean install
+run_bench artemis-odb-0.4.0
+run_bench artemis-odb-0.6.5
+run_bench artemis-odb-0.7.0
+run_bench ashley-1.0.1
+run_bench ashley-1.2.0
+run_bench gdx-artemis-0.5.0
+mvn -Pfast clean install -f artemis-odb-0.7.0/pom.xml
+run_bench artemis-odb-0.7.0
