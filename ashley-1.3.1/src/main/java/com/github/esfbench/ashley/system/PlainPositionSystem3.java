25efa42e88e9e5ebd1c2dde9820c226809efe414
Piotr J <piotrj.dev@gmail.com>
2015-02-01 13:07:22 +0100

    fixed ashley becnhmarks

--- ashley-1.3.1/src/main/java/com/github/esfbench/ashley/system/PlainPositionSystem3.java
+++ ashley-1.3.1/src/main/java/com/github/esfbench/ashley/system/PlainPositionSystem3.java
@@ -4,12 +4,14 @@ package com.github.esfbench.ashley.system;
 import org.openjdk.jmh.infra.Blackhole;
 
 import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.github.esfbench.ashley.component.PlainPosition;
 
 public class PlainPositionSystem3 extends IteratingSystem {
 
 	Blackhole voidness = new Blackhole();
+	ComponentMapper<PlainPosition> positionMapper = ComponentMapper.getFor(PlainPosition.class);
 	
 	@SuppressWarnings("unchecked")
 	public PlainPositionSystem3() {
@@ -18,7 +20,7 @@ public class PlainPositionSystem3 extends IteratingSystem {
 	
 	@Override
 	public void processEntity (com.badlogic.ashley.core.Entity e, float deltaTime) {
-		PlainPosition pos = e.getComponent(PlainPosition.class);
+		PlainPosition pos = positionMapper.get(e);
 		pos.x += 0.5f;
 		pos.y -= 0.5f;
 		
