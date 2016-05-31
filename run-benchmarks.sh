e1f94060f41d8b49b870c33d3ec70aab622cbdd7
Adrian Papari <a@onedaybeard.net>
2016-05-31 18:07:48 +0200

    running RC1, RC2 benchmarks

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -10,7 +10,7 @@
 #PARAMS="-i 3 -wi 3 -r 5 -p entityCount=16384,262144"
 #PARAMS="-i 3 -wi 5 -r 5 -p entityCount=16384,65536,262144"
 #PARAMS="-i 5 -wi 5 -r 5 -p entityCount=16384,65536,262144"
-PARAMS="-i 3 -wi 5 -r 5 -p entityCount=65536,262144"
+#PARAMS="-i 3 -wi 5 -r 5 -p entityCount=65536,262144"
 #PARAMS="-i 1 -wi 1 -r 1 -p entityCount=262144"
 
 
@@ -19,9 +19,10 @@ function run_bench() {
 #	java -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -jar $1/target/microbenchmarks.jar ".*" -e ".*(baseline|packed|plain|legacy).*" -rf json -rff results/$1$2.json $PARAMS | tee results/$1$2.log
 #		-prof stack -prof hs_rt \
 	java -jar $1/target/microbenchmarks.jar \
-		".*(insert|transmuter|pool)" \
-		-e ".*(baseline|packed|plain|legacy).*" \
+		".*" \
 		-rf json -rff results/$1$2.json $PARAMS | tee results/$1$2.log
+#		-e ".*(baseline|packed|plain|legacy).*" \
+
 #	java -XX:+UnlockCommercialFeatures \
 #		-XX:+FlightRecorder \
 #		-XX:StartFlightRecording=duration=60s,filename=./profiling-data.jfr,name=FULL,settings=profile \
@@ -41,15 +42,18 @@ function run_all() {
 	run_bench artemis-odb-0.9.0
 	run_bench artemis-odb-1.0.1
 	run_bench artemis-odb-2.0.0
+	run_bench artemis-odb-2.0.0-RC1 
+	run_bench artemis-odb-2.0.0-RC2
 	run_bench ashley-1.6.0
 	run_bench gdx-artemis-0.5.0
 	run_bench retinazer-0.2.0
 
 	# recompiling with bytecode optimizations
 	mvn -P fast clean install
-	run_bench artemis-odb-0.9.1 _fast
+	run_bench artemis-odb-0.9.0 _fast
 	run_bench artemis-odb-1.0.1 _fast
-	run_bench artemis-odb-2.0.0 _fast
+	run_bench artemis-odb-2.0.0-RC1 _fast
+	run_bench artemis-odb-2.0.0-RC2 _fast
 }
 
 
@@ -59,6 +63,6 @@ function run_dev() {
 	run_bench artemis-odb-2.0.0-RC2 _fast
 }
 
-#run_all
-run_dev
+run_all
+#run_dev
 
